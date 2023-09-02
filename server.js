e0fa98c47f7853c9f26e3beb96b5683c468b1740
Noah Salmon <noahsalmon98@gmail.com>
2023-09-02 14:44:19 -0400

    added middleware

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+
+//Middleware
+app.use(express.json());
+app.use(express.static('public'));
