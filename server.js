e47d5e5ec9537c49488a4a7558d5dc16204610f3
Noah Salmon <noahsalmon98@gmail.com>
2023-09-02 14:50:45 -0400

    set up server

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const fs = require('fs');
 //Middleware
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+app.get('/notes', (req,res)=> {
+    res.sendFile(path.join(__dirname ,'/public/notes.html'));
+})
+app.listen(PORT, ()=> {
+    console.log(`Express listening to Port : ${PORT}`);
+})
+
